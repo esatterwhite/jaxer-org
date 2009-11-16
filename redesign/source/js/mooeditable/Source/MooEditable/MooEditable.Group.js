f95527b8f4aa951f694720548adb14cac9986c9b
esatterwhite <esatterwhite@wi.rr.com>
2009-11-16 09:43:35 -0600

    static files for debugging and design work
    Signed-off-by: esatterwhite <esatterwhite@wi.rr.com>

---
+++ redesign/source/js/mooeditable/Source/MooEditable/MooEditable.Group.js
@@ -0,0 +1,70 @@
+/*
+---
+
+script: MooEditable.Group.js
+
+description: Extends MooEditable to have multiple instances on a page controlled by one toolbar.
+
+license: MIT-style license
+
+authors:
+- Ryan Mitchell
+
+requires:
+- core:1.2.4/Options
+- /MooEditable
+- /MooEditable.UI
+- /MooEditable.Actions
+
+provides: [MooEditable.Group]
+
+...
+*/
+
+MooEditable.Group = new Class({
+
+	Implements: [Options],
+	
+	options: {
+		actions: 'bold italic underline strikethrough | insertunorderedlist insertorderedlist indent outdent | undo redo | createlink unlink | urlimage | toggleview'
+	},
+    
+	initialize: function(toolbarEl, options){
+		this.setOptions(options);
+		this.actions = this.options.actions.clean().split(' ');
+		var self = this;
+		this.toolbar = new MooEditable.UI.Toolbar({
+			onItemAction: function(){
+				var args = $splat(arguments);
+				var item = args[0];
+				if (!self.activeEditor) return;
+				self.activeEditor.focus();
+				self.activeEditor.action(item.name, args);
+				if (self.activeEditor.mode == 'iframe') self.activeEditor.checkStates();
+			}
+		}).render(this.actions);
+		document.id(toolbarEl).adopt(this.toolbar);
+	},
+
+	add: function(textarea, options){
+		return this.activeEditor = new MooEditable.Group.Item(textarea, this, $merge({toolbar: false}, this.options, options));
+	}
+	
+});
+
+
+MooEditable.Group.Item = new Class({
+
+	Extends: MooEditable,
+
+	initialize: function(textarea, group, options){
+		this.group = group;
+		this.parent(textarea, options);
+		var focus = function(){
+			this.group.activeEditor = this;
+		}.bind(this);
+		this.textarea.addEvent('focus', focus);
+		this.win.addEvent('focus', focus);
+	}
+
+});
