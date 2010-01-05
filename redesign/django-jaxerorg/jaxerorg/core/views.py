3ec336ca72a21fd5d95704c478d563270efa80b8
esatterwhite <esatterwhite@wi.rr.com>
2010-01-05 08:49:20 -0600

    white space cleanup and remove pdb statements
    Signed-off-by: esatterwhite <esatterwhite@wi.rr.com>

--- redesign/django-jaxerorg/jaxerorg/core/views.py
+++ redesign/django-jaxerorg/jaxerorg/core/views.py
@@ -6,28 +6,21 @@ from django.utils.safestring import EscapeString
 from django.template.defaultfilters import force_escape
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
-
 from messages.views import compose
 from jaxerprofile.forms import MultiUserComposeForm
+
 def escape_code(request, code=""):
     return HttpResponse(EscapeString(request.POST['code']))
-
 def jaxer_home(request):
-    
     return render_to_response('core/homepage.html', 
                              {}, 
                              context_instance=RequestContext(request))
-
 def editor_test(request):
     from jaxerorg.core.forms import EditorForm
     form = EditorForm()
     return render_to_response('core/editortest.html', {'form':form}, context_instance=RequestContext(request))
-def search_test(request):
-
-    
+def search_test(request):    
     if request.POST:
-        import pdb
-        pdb.set_trace()
         form = MultiUserComposeForm(request.POST)
         if form.is_valid():
             form.save(sender=request.user)
@@ -41,16 +34,10 @@ def multi_mail(request):
      return compose(request, form_class=MultiUserComposeForm, template_name='messages/multiusercompose.html') 
 def ajax_code_form(request):
     ''' returns HTML code for InsertCodeForm as an unordered list'''
-    
     from jaxerorg.core.forms import InsertCodeForm
-
-    
     form = InsertCodeForm()
-    
     return HttpResponse(form.as_ul())
-
 def login_user(request, next='/'):
-
     from jaxerorg.core.forms import LoginForm
     message=None
     if request.is_ajax():
@@ -75,9 +62,7 @@ def login_user(request, next='/'):
                     return render_to_response('core/login.html', {'form':form,'message':message}, context_instance=RequestContext(request))
         else:
             form = LoginForm( initial={'next':request.META['HTTP_REFERER']})
-            
             return render_to_response('core/login.html', {'form':form}, context_instance=RequestContext(request))
-        
 def logout_user(request):
     logout(request)
     return HttpResponseRedirect('/')
